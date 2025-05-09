d1b3841e1c9a220ebb1610903fed52e70c146fa4
David Stepanic <davidstepanic@Davids-MacBook-Pro.local>
2025-05-09 09:27:29 +0200

    Hero section completed

---
+++ types/index.ts
@@ -0,0 +1,9 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+    title: string;
+    containerStyles? : string;
+    handleClick?: 
+    MouseEventHandler<HTMLButtonElement>;
+    //  specificirali smo da to bude eventhandler nad htmlbutton element
+}
